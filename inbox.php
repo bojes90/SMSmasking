f6193fe7c677337878db7c9c21c964ea5992459c
bojes90 <50340804+bojes90@users.noreply.github.com>
2019-07-26 05:36:00 +0700

    Inbox
    Pesan Masuk

---
+++ inbox.php
@@ -0,0 +1,33 @@
+{
+  "recordsTotal": 50,
+  "recordsFiltered": 50,
+  "data": [
+    {
+      "no": "1",
+      "id": "9ccd99d1957010e93e07c3fbafd292750",
+      "sudah_baca": "1",
+      "tglterima": "02 Apr 2018/10:15:27",
+      "nama_pengirim": null,
+      "no_pengirim": "081264xxxxx",
+      "isipesan": "SMS Dari Bagas"
+    },
+    {
+      "no": "2",
+      "id": "091d2b608fd54d5bea642982d95cae6b4",
+      "sudah_baca": "1",
+      "tglterima": "03 Apr 2018/10:02:36",
+      "nama_pengirim": null,
+      "no_pengirim": "081265xxxxx",
+      "isipesan": "Saya akan hadir tepat waktu"
+    },
+    {
+      "no": "3",
+      "id": "20b82c95b501bacd1e776864209b17053",
+      "sudah_baca": "0",
+      "tglterima": "05 Apr 2018/13:37:56",
+      "nama_pengirim": "Rudi",
+      "no_pengirim": "081266xxxxx",
+      "isipesan": "Terima kasih ats infonya"
+    }
+  ]
+}
